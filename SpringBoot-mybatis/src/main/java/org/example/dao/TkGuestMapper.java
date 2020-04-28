b681c43666b78b65b0831ec14c59e9c5cba63db6
likedust@126.com <${likedust@126.com}>
2020-04-28 15:19:20 +0800

    SpringBoot学习之与Mybatis增强插件tk-mybatis的使用，以及解决部分依赖冲突

--- SpringBoot-mybatis/src/main/java/org/example/dao/TkGuestMapper.java
+++ SpringBoot-mybatis/src/main/java/org/example/dao/TkGuestMapper.java
@@ -6,7 +6,7 @@ import tk.mybatis.mapper.common.Mapper;
 /**
  * @Author: aixiaolin
  * @Date: 2020-04-28 14:05
- * @Description:
+ * @Description: 这是继承自tk-mybatis插件的mapper，常用的对数据库操作方法都可以省略
  */
 public interface TkGuestMapper extends Mapper<Guest> {
 
